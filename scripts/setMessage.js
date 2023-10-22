6b5cfc79413bc16a6238ca1459028b61fc2856c4
your_username <stztrujillo@hotmail.com>
2023-10-22 07:29:18 +0100

    updates scripts

--- scripts/setMessage.js
+++ scripts/setMessage.js
@@ -1,20 +1,23 @@
 const { abi, bytecode } = require("../artifacts/contracts/cs.sol/cs.json");
 const { Web3 } = require("web3");
+const web3utils = require("web3-utils");
 require("dotenv").config();
 
-const web3 = new Web3(new Web3.providers.HttpProvider(process.env.sepolia));
-
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.sepolia_infura));
+console.log("SEPOLIA", process.env.sepolia_infura);
 async function main() {
-  //get signer
+  // Initiate wallet/signer
   const account = web3.eth.accounts.wallet.add(process.env.PK).get(0);
-  //get contract
-  const address = "0x6EC591AD3e6eAC0CB48B8eF5022E1a70c6f08F04";
+  console.log("SEPOLIA", process.env.PK);
+
+  // Get contract
+  const address = "0x50aCdC8D7BcbbbD19D1d145bDc941964b8822818";
   const myContract = new web3.eth.Contract(abi, address);
 
-  //set message function
+  // Use contract method to send a transaction
   const msg = "Hello CS1";
-  const tx = await myContract.methods.setMessage(msg).send({ from: account?.address });
-  console.log("DONE", tx);
+  const tx = await myContract.methods.setMessage(msg).send({ from: account.address });
+  console.log("Transaction Hash:", tx.transactionHash);
 }
 
 main();
