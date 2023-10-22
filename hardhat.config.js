031f6eac35be8df4b0487777d598f68311dfb13b
your_username <stztrujillo@hotmail.com>
2023-10-22 07:29:34 +0100

    updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "sepolia",
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `${process.env.sepolia}`,
+      accounts: [`${process.env.PK}`],
+    },
+    ganache: {
+      url: `${"HTTP://127.0.0.1:7545"}`,
+      accounts: [`0xd0f458f27c80b2e067c83a9ca19b7331eb39b5dd5c90bd2d02af5f39f2382005`],
+    },
+  },
 };
