d7bddd09be0cd94210c6d7e7c7b51c73eb39da62
your_username <stztrujillo@hotmail.com>
2023-10-22 13:32:16 +0100

    added events(not working)

---
+++ scripts/web3-eth-events.js
@@ -0,0 +1,17 @@
+const { Web3 } = require("web3");
+const abi = require("../artifacts/contracts/tether.json");
+require("dotenv").config();
+
+const web3 = new Web3(process.env.sepolia);
+
+async function main() {
+  //initialize contract
+  const contractAddress = "0x7169D38820dfd117C3FA1f22a697dBA58d90BA06";
+  const contract = new web3.eth.Contract(abi, contractAddress);
+
+  console.log("EVENTS", contract.events);
+  const subscription = contract.events.Transfer();
+  //subscription.on("data", console.log);
+}
+
+main();
