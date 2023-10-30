2682d60edfee25292d34afcfc6c94a94cb7c1aa7
your_username <stztrujillo@hotmail.com>
2023-10-30 20:05:41 +0000

    changed contract address

--- scripts/web3-eth-getMessage.js
+++ scripts/web3-eth-getMessage.js
@@ -1,6 +1,5 @@
 const { abi, bytecode } = require("../artifacts/contracts/cs.sol/cs.json");
 const { Web3 } = require("web3");
-const web3utils = require("web3-utils");
 require("dotenv").config();
 
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.sepolia));
@@ -9,7 +8,7 @@ async function main() {
   //get account
 
   //get contract
-  const contractAddress = "0x7F98c91c95757B84c8c40A78754242C14Bfb6075";
+  const contractAddress = "0x50aCdC8D7BcbbbD19D1d145bDc941964b8822818";
   const myContract = new web3.eth.Contract(abi, contractAddress);
 
   //call function w web3js call
